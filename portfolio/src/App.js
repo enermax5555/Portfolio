bac83dc3adcc62c10396d473a32fa63d93ae79dd
RedFlash <stoqn.sisimen@gmail.com>
2022-11-22 20:38:46 +0200

    Restored WebVitals, Added Fonts, created plain navigation bar component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,1 +1,14 @@
+import NavBar from "./components/navbar";
 
+
+function App() {
+  return(
+<main>
+<NavBar>
+
+</NavBar>
+</main>
+  );
+
+}
+export default App;
---
+++ portfolio/src/components/navbar.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function NavBar() {
+    <p class="text-black-200">Hello, let me check!</p>
+}
