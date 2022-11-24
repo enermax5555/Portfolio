b6559b5bbe00c0c201aef12d93fdeea3b0731916
RedFlash <stoqn.sisimen@gmail.com>
2022-11-24 22:18:36 +0200

    Recustomization the navigation bar, added IconsNav.js

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
+
 import { useState } from "react";
+import Icons from "./IconsNav";
 
 export default function NavBar() {
     const [arrow, setArrow] = useState(true);
@@ -7,23 +9,33 @@ export default function NavBar() {
 
         <main className='flex text-white cursor-default'>
 <header className="flex space-x-10 p-1">
-    <div className='text-7xl'>
-        <div className='shadow-sm shadow-black'> S </div>
-    </div>
+    <div className='bg-black p-2'>
+        <div className='absolute pl-6 text-red-500 text-7xl '>S</div>
+        <div className='text-cyan-400 pl-5 text-7xl '> S </div>
+        <p className='text-center text-base pb-2'>Stoqn</p>
+        <p className='text-center text-xs text-gray-400'>Web Developer</p>
+        <hr className='mt-3'/>
+        <nav className={`${arrow ? 'h-auto w-auto text-center text-gray-600 pt-5 pt-5 pb-5' : 'invisible opacity-0 h-0 w-0 cursor-normal text-transperant'} duration-1000`}>
+        <ul className='hover:text-black hover:bg-white cursor-pointer p-1'> Home </ul><hr/>
+    <ul className='hover:text-black hover:bg-white cursor-pointer p-1'> About me</ul><hr/>
+    <ul className='hover:text-black hover:bg-white cursor-pointer p-1'> Projects</ul><hr/>
+    <ul className='hover:text-black hover:bg-white cursor-pointer p-1'> Contacts</ul><hr/>
+        <Icons/>
+        <hr/>
+        </nav>
+        <div className='pt-3 mb-3 hover:animate-pulse hover:text-white-600 duration-100'>
 
-    <button className={`${arrow ? "h-8" : "h-15"} duration-600 border-x-8 border-y-transparent border-b-[1.2rem] border-b-white-600 cursor-pointer`} 
-     onClick={() => setArrow(!arrow)}
-    //  onClick={() => console.log("what's up hoomie?")}
-    />
+            <button onClick={() => setArrow(!arrow)}>
+            
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+        </svg>
+        </button>
+        </div>
 
-    
-    
-    <nav className={`${arrow ? 'invisible flex space-x-2 items-center' : 'flex text-2xl items-center space-x-8'} duration-700`}>
-    <ul className='hover:text-black hover:bg-white cursor-pointer'> Home </ul>
-    <ul className='hover:text-black hover:bg-white cursor-pointer'> About me</ul>
-    <ul className='hover:text-black hover:bg-white cursor-pointer'> Projects</ul>
-    <ul className='hover:text-black hover:bg-white cursor-pointer'> Contacts</ul>
-    </nav>
+        <hr/>
+        
+        </div>
     </header>
     </main>
     );
