c10e428926e4fcdd025bc8504a3aca1ea8d81804
RedFlash <stoqn.sisimen@gmail.com>
2022-12-05 17:20:57 +0200

    Added new styling on the Home.js.

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,25 +1,64 @@
+import { hover } from "@testing-library/user-event/dist/hover"
 import React from "react"
 import ImageHome from "./ImageHome"
 
 export default function Home() {
     return(
-
+        
         <main id='Home'>
             <selection className='flex pt-10 justify-center select-none'>
             <p className='font-mono text-2xl text-white font-bold'></p>
             </selection>
             <header className='flex gap-x-5 lg:pl-40 lg:pt-10 pl-5 text-center select-none'>
             <div className='space-y-3 text-white font-poppins shadow-sm'> 
-               <p className='text-6xl pb-5'>Hello there!</p> 
-                <p className='text-4xl'>My name is</p>
-                <div className='flex'>
+               <h1 className='text-6xl pb-5 hover:scale-125 duration-700'>
+               <span className='test'>H</span>
+               <span className='test'>e</span>
+               <span className='test'>l</span>
+               <span className='test'>l</span>
+               <span className='test'>o</span>
+               <span className='test tab'>T</span>
+               <span className='test'>h</span>
+               <span className='test'>e</span>
+               <span className='test'>r</span>
+               <span className='test'>e</span></h1>
+                <h1 className='text-4xl hover:scale-125 duration-700'>
+                    <span className='test'>M</span>
+                    <span className='test'>y</span>
+                    <span className='test tab'>n</span> 
+                    <span className='test'>a</span>
+                    <span className='test'>m</span>
+                    <span className='test'>e</span> 
+                    <span className='test tab'>i</span> 
+                    <span className='test'>s</span>
+                    </h1>
+                <div className='flex hover:scale-125 duration-700'>
                 <p className='absolute text-6xl text-red-500 pl-1 font-bold'>S</p>
                 <p className='text-6xl text-cyan-500 font-bold'>S</p>
                 <div className='pt-3.5'>
-                <p className='text-4xl text-right text-end'>toqn,</p>
+                <h1 className='text-4xl text-right text-end'>
+                    <span className='test'>t</span>
+                    <span className='test'>o</span>
+                    <span className='test'>q</span>
+                    <span className='test'>n</span>
+                    <span >,</span>
+                </h1>
                 </div>
                 </div>
-                <p className='text-4xl'>web developer</p>
+                <h1 className='text-4xl hover:scale-125 duration-700'>
+                    <span className='test'>w</span>
+                    <span className='test'>e</span>
+                    <span className='test'>b</span>
+                    <span className='test tab'>d</span>
+                    <span className='test'>e</span>
+                    <span className='test'>v</span>
+                    <span className='test'>e</span>
+                    <span className='test'>l</span>
+                    <span className='test'>o</span>
+                    <span className='test'>p</span>
+                    <span className='test'>e</span>
+                    <span className='test'>r</span>
+                    </h1>
                 </div>
                 <div className='flex hidden lg:contents'>
                 <img className='w-20 m-10' src={require('../Assets/images/jonyTheExplorerWithRemovedBg.png')} alt='Explorer Web Dev' />
