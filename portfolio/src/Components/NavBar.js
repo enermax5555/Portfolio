06b1f0ed190252a0689ac4f8427ebac6a0b16433
RedFlash <stoqn.sisimen@gmail.com>
2022-12-08 17:55:12 +0200

    Added new fonts and some styling changes

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -12,14 +12,14 @@ export default function NavBar() {
 <header className="flex space-x-10 p-1">
     <div className='w-80 lg:w-auto bg-black p-2 select-none'>
         <div className='hidden lg:contents'>
-        <div className='absolute pl-6 text-red-500 text-7xl'>S</div>
-        <div className='text-cyan-400 pl-5 text-7xl'> S </div>
+        <div className='absolute pl-5 text-red-500 text-7xl'>S</div>
+        <div className='text-cyan-400 pl-4 text-7xl'> S </div>
         </div>
-        <p className='text-center text-base pb-2'>Stoqn</p>
-        <p className='text-center text-xs text-gray-400'>Web Developer</p>
+        <p className='text-center text-base pb-2 font-montserrat'>Stoqn</p>
+        <p className='text-center text-xs text-gray-400 font-montserrat'>Web Developer</p>
         <hr className='mt-3'/>
 
-        <nav className={`${arrow ? 'h-auto w-auto text-center text-gray-600 pt-5 pt-5 pb-5' : 'collapse pb-5 scale-75 text-center w-auto opacity-0 h-0 w-0 cursor-normal'} duration-300`}>
+        <nav className={`${arrow ? 'h-auto w-auto text-center text-gray-600 pt-5 pt-5 pb-5' : 'collapse pb-5 scale-75 text-center w-auto opacity-0 h-0 w-0 cursor-normal'} font-montserrat duration-300`}>
         <AnchorLink href='#Home'>
         <ul className='hover:text-black hover:bg-white cursor-pointer p-1'>
             Home
