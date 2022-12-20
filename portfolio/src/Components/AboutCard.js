839535ab60fd95a4411590fcc3c109c754eba3bd
RedFlash <stoqn.sisimen@gmail.com>
2022-12-20 16:05:41 +0200

    Added an animation to the flipcard in AboutCard.js

--- portfolio/src/Components/AboutCard.js
+++ portfolio/src/Components/AboutCard.js
@@ -83,19 +83,19 @@ export default function AboutCard() {
     })
 
     const BackCard = BackCardStacks.map((e) => {
-        return(
-        <div>
-            <img className='m-auto rounded-full w-7' src={e.src} alt={e.alt} />
-            <p className='p-3 font-montserrat'><b>{e.label}</b><br />{e.t1}<br />{e.t2}</p>
-        </div>
+        return (
+            <div>
+                <img className='m-auto rounded-full w-7' src={e.src} alt={e.alt} />
+                <p className='p-3 font-montserrat'><b>{e.label}</b><br />{e.t1}<br />{e.t2}</p>
+            </div>
         )
     })
-    
-    return(
+
+    return (
         <main>
             <div className='group m-10'>
-                <div className=''>
-                    <div className={`${flipcard ? 'hidden' : ''}`}>
+                
+                    <div className={`${flipcard ? 'group-first:rotate-y-full' : 'ml-7'} absolute backface-hidden duration-1000`}>
                         <p className='text-center py-1 font-bold text-xl pb-2 font-montserrat'>Skills</p>
                         <img className='m-auto rounded-full w-7' src={skillIMG} alt='My Skils' />
                         <div className='grid grid-cols-4 pt-10'>
@@ -103,23 +103,23 @@ export default function AboutCard() {
                         </div>
                     </div>
 
-                        <div className={`${flipcard ? '' : 'hidden'}`}>
-                            <p className='text-center py-1 font-bold text-xl pb-2 font-montserrat'>Skills</p>
-                            <img className='m-auto rounded-full w-7' src={skillIMG} alt='My Skils' />
-                            <div className='grid grid-cols-4 pt-10'>
+                    <div className={`${flipcard ? '' : 'group-last:rotate-y-full'} backface-hidden duration-1000`}>
+                        <p className='text-center py-1 font-bold text-xl pb-2 font-montserrat'>Skills</p>
+                        <img className='m-auto rounded-full w-7' src={skillIMG} alt='My Skils' />
+                        <div className='grid grid-cols-4 pt-10'>
                             {BackCard}
-                            </div>
                         </div>
-                </div>
+                    </div>
+
                 <div className='mt-10'>
-            <button type="button"
-                    onClick={() => setflipcard(!flipcard)} 
-                    class="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-40
+                    <button type="button"
+                        onClick={() => setflipcard(!flipcard)}
+                        class="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-40
                             bg-gradient-to-br from-purple-700 to-blue-700
                             hover:bg-gradient-to-bl">{`${flipcard ? 'Back' : 'Check other'}`}</button>
-                               
+
                 </div>
             </div>
-        </main>
+        </main >
     )
 }
